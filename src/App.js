b7addb039b0c4dfd2b96407941b1b76c0c97f34b
Vanessa Barbiero <vbarbiero0@gmail.com>
2020-12-23 10:29:58 -0500

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">Weather App
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Vanessa Barbiero and is{" "}
+      <a href="https://github.com/vbarbiero0/react-weather-app"
+      target="_blank">
+         open-sourced on Github
+      </a>
+      </footer>
     </div>
   );
 }
