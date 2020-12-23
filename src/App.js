58196ccbb16651b1b53ca975fe119b5fb03699d2
Vanessa Barbiero <vbarbiero0@gmail.com>
2020-12-23 11:21:15 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by Vanessa Barbiero and is{" "}
@@ -13,6 +14,7 @@ export default function App() {
          open-sourced on Github
       </a>
       </footer>
+      </div>
     </div>
   );
 }
